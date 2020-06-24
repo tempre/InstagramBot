a5d8daccb38967990672afcea1547e8517501219
Anthony Schreiber <schreiberanthony72@gmail.com>
2020-06-24 19:04:49 -0400

    Check Followers

--- InstaBot/Drivers/driver.py
+++ InstaBot/Drivers/driver.py
@@ -2,5 +2,4 @@ from selenium import webdriver
 
 def driver():
     driver = webdriver.Opera(executable_path = 'Atom\InstagramBot\InstagramBot\InstaBot\Drivers\operadriver.exe')
-    driver.get('https://www.instagram.com/')
     return driver
--- InstaBot/main.py
+++ InstaBot/main.py
@@ -6,11 +6,13 @@ from Logins import Login
 um = Login.LoginUM()
 pw = Login.LoginPW()
 
+
 class InstaBot:
 
     def __init__(self, um, pw):
         self.browser = driver.driver();
 
+        self.browser.get('https://www.instagram.com/')
         #pragma region Login
         sleep(4)
         self.browser.find_element_by_xpath('//input[@name=\"username\"]')\
@@ -33,7 +35,15 @@ class InstaBot:
         if self.browser.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/h2"):
             self.browser.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
             .click()
-            
+
         #pragma end
 
+    def getFollowers(self, um):
+        self.browser.get("https://www.instagram.com/" + um + "/")
+        sleep(2)
+
+        self.browser.find_element_by_xpath("//a[contains(@href,'/follower')]")\
+        .click()
+
 botIM = InstaBot(um, pw)
+botIM.getFollowers(um)
