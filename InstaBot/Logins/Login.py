f3916df9a679fec744ccf783ac385f524adf742d
Anthony Schreiber <schreiberanthony72@gmail.com>
2020-06-24 17:04:02 -0400

    using checks for popups

--- InstaBot/Logins/Login.py
+++ InstaBot/Logins/Login.py
@@ -2,6 +2,6 @@ loginFile = open("Atom\InstagramBot\InstagramBot\InstaBot\Logins\Login.txt", "r"
 
 def LoginUM():
     return loginFile.readline()
-    
+
 def LoginPW():
     return loginFile.readline()
--- InstaBot/main.py
+++ InstaBot/main.py
@@ -9,26 +9,31 @@ pw = Login.LoginPW()
 class InstaBot:
 
     def __init__(self, um, pw):
-        #self.driver = webdriver.Opera(executable_path = 'Atom\InstagramBot\InstagramBot\InstaBot\Drivers\operadriver.exe')
-
         self.browser = driver.driver();
 
+        #pragma region Login
         sleep(4)
         self.browser.find_element_by_xpath('//input[@name=\"username\"]')\
         .send_keys(um)
 
         self.browser.find_element_by_xpath('//input[@name=\"password\"]')\
         .send_keys(pw)
+        #pragma regionend
 
-        self.browser.find_element_by_xpath('//button[@type="submit"]')\
-        .click()
+        #pragma region Instagram Auth Junk
 
-        sleep(3)
-        self.browser.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-        .click()
+        #checking for instagram popup for saving login info
+        sleep(2)
+        if self.browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/section/div/div[2]"):
+                self.browser.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+                .click()
 
-        sleep(3)
-        self.browser.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-        .click()
+        #checking for instagram popup for notifcations
+        sleep(2)
+        if self.browser.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/h2"):
+            self.browser.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
+            .click()
+            
+        #pragma end
 
 botIM = InstaBot(um, pw)
