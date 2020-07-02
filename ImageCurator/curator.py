61bef193eac69c8013bce2e127c3265fb11c9448
Anthony Schreiber <schreiberanthony72@gmail.com>
2020-07-02 03:00:42 -0400

    fixes

--- ImageCurator/curator.py
+++ ImageCurator/curator.py
@@ -3,10 +3,17 @@ import glob
 import urllib.request
 from time import sleep
 from selenium import webdriver
+from PIL import Image
+
+#region Params
+
+author_value = 'data-pin-url'
+#endregion
 
 #region URL |Experimental |
 
 url = 'https://www.tumblr.com/search/%2335mm+film'
+#url = 'https://www.pinterest.com/search/pins/?q=%2335mm%20film&rs=typed&term_meta[]=%2335mm%7Ctyped&term_meta[]=film%7Ctyped'
 
 #endregion
 
@@ -36,6 +43,7 @@ range_for_scroll = 10
 #region Grab Images and Authors and save them
 
 while True:
+
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
     sleep(1)
@@ -43,31 +51,42 @@ while True:
     new_height = browser.execute_script("return document.body.scrollHeight")
 
     try:
-        images = browser.find_elements_by_xpath('//img[@class="photo"]')
+        #images = browser.find_elements_by_xpath('//*[@class="%s"]' % active_value)
+        images = browser.find_elements_by_tag_name("img")
+        print(str(images))
+
         for image in images:
             image_src = images[number].get_attribute('src')
+
             author_src = images[number].get_attribute('data-pin-url')
+
             new_src = []
+
+#not allowing gifs for my use
+
             if "gif" not in image_src:
                 new_src = image_src
-            if len(new_src) != 0:
-                print(str(id) + " Image")
-                print('IMAGE = ' + new_src)
-                print('SIZE = ' + str(images[number].size))
-                print('AUTHOR = ' + author_src + '\n')
+                if len(new_src) != 0:
+                    print(str(id) + " Image")
+                    print('IMAGE = ' + new_src)
+                    print('SIZE = ' + str(images[number].size))
+                    print('AUTHOR = ' + str(author_src) + '\n')
 
-                urllib.request.urlretrieve(''.join(new_src), 'ImageDownloadTEMP/' + str(id) + '.jpg')
-                f = open('ImageDownloadTEMP/image_authors.txt',"a")
-                f.write("Image " + str(id) + ": " + str(author_src) + "\n")
-                f.close()
-                id += 1
+                    dict = images[number].size
+                    width = dict.get('width')
+                    height = dict.get('height')
+
+                    if width > 150 and height > 150:
+                        urllib.request.urlretrieve(''.join(new_src), 'ImageDownloadTEMP/' + str(id) + '.jpg')
+                        f = open('ImageDownloadTEMP/image_authors.txt',"a")
+                        f.write("Image " + str(id) + ": " + str(author_src) + "\n")
+                        f.close()
+                    id += 1
             number += 1
     except IndexError:
         print("Waiting for page scroll...")
 
-    new_height = browser.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break
     last_height = new_height
-
 #endregion
